a4292c94badd74d16566ee7295b618ab4ce4ad57
KangJi <kjhmd9726@gmail.com>
2021-06-16 02:24:45 +0900

    [refactor] character 객체 property 이름 간소화

--- src/socket/character/index.js
+++ src/socket/character/index.js
@@ -1,7 +1,7 @@
 module.exports = (io, socket, characters) => {
   characters[socket.id] = {
-    xCoordinate: 450,
-    yCoordinate: 350,
+    x: 450,
+    y: 350,
     animation: 'turn',
     socketId: socket.id,
   };
@@ -11,8 +11,8 @@ module.exports = (io, socket, characters) => {
   socket.broadcast.emit('character:newCharacter', characters[socket.id]);
 
   socket.on('character:move', (movementData) => {
-    characters[socket.id].xCoordinate = movementData.xCoordinate;
-    characters[socket.id].yCoordinate = movementData.yCoordinate;
+    characters[socket.id].x = movementData.x;
+    characters[socket.id].y = movementData.y;
     characters[socket.id].animation = movementData.animation;
 
     socket.broadcast.emit('character:moved', characters[socket.id]);
