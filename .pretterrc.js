d1d3e4cb4d14ed3857ad4a433a4d0767cf871fe5
joi0104 <jjjjoi0104@gmail.com>
2021-07-29 11:49:32 +0900

    [chore] prettierrc 오타 수정 및 eslint가 돌지 않는 곳에서도 돌게 수정

--- .pretterrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    singleQuote: true,
-    semi: true,
-    tabWidth: 2,
-    trailingComma: 'all',
-    printWidth: 100,
-    arrowParens: 'avoid',
-    endOfLine: 'auto'
-};
--- src/socket/character/index.js
+++ src/socket/character/index.js
@@ -1,21 +1,11 @@
 module.exports = (socket, characterGroup, character) => {
-  socket.emit(
-    "character:currentCharacter",
-    characterGroup.getCharacterGroupState()
-  );
+  socket.emit('character:currentCharacter', characterGroup.getCharacterGroupState());
 
-  socket.broadcast.emit(
-    "character:newCharacter",
-    character.getCharacterState()
-  );
+  socket.broadcast.emit('character:newCharacter', character.getCharacterState());
 
-  socket.on("character:move", (movementData) => {
-    character.setCharacterState(
-      movementData.x,
-      movementData.y,
-      movementData.animation
-    );
+  socket.on('character:move', movementData => {
+    character.setCharacterState(movementData.x, movementData.y, movementData.animation);
 
-    socket.broadcast.emit("character:moved", character.getCharacterState());
+    socket.broadcast.emit('character:moved', character.getCharacterState());
   });
 };
