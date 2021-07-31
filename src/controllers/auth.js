e542adeb79db6ea7e332ba745f04253a9b64c1b6
joi0104 <jjjjoi0104@gmail.com>
2021-08-01 00:12:16 +0900

    [feat] oAuthToken 이나 vendor가 오지 않을 경우 처리

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -4,9 +4,13 @@ exports.signin = async (request, response) => {
   try {
     const oAuthToken = request.headers.authorization;
     const { vendor } = request.query;
-    const id = await AuthService.validateToken(vendor, oAuthToken);
+    if (!oAuthToken || !vendor) {
+      throw new Error('no oAuthToken or vendor');
+    }
+    const id = await AuthService.validateOAuthToken(vendor, oAuthToken);
     response.json({ id });
   } catch (err) {
+    console.log(err);
     response.status(500).json(err);
   }
 };
