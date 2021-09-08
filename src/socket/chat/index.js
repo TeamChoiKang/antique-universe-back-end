5eb8c8689d579a4973c2c5dafae550408cd6936a
joi0104 <jjjjoi0104@gmail.com>
2021-09-09 02:50:46 +0900

    [feat] chat 메시지를 room에게만 전송하도록 변경

--- src/socket/chat/index.js
+++ src/socket/chat/index.js
@@ -1,5 +1,5 @@
 module.exports = (socket, character) => {
   socket.on('chat:createNewChat', newChat => {
-    socket.broadcast.emit('chat:getNewChat', newChat);
+    socket.to(character.getCurrentScene().getName()).emit('chat:getNewChat', newChat);
   });
 };
