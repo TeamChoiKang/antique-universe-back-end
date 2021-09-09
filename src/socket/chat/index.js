e7abd2260be095d73d59f70c359a63069ace3b57
joi0104 <jjjjoi0104@gmail.com>
2021-09-10 00:02:10 +0900

    [feat] chat 메세지에 author 추가

--- src/socket/chat/index.js
+++ src/socket/chat/index.js
@@ -1,5 +1,8 @@
 module.exports = (io, socket, character) => {
   socket.on('chat:createNewChat', newChat => {
-    io.in(character.getCurrentScene().getName()).emit('chat:getNewChat', newChat);
+    io.in(character.getCurrentScene().getName()).emit('chat:getNewChat', {
+      author: socket.handshake.query.nickname,
+      message: newChat,
+    });
   });
 };
