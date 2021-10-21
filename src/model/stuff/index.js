1537172e37be100f54feee5c307ade14b09e989a
KangJi <kjhmd9726@gmail.com>
2021-10-21 22:43:33 +0900

    [feat] stuff model 추가

---
+++ src/model/stuff/index.js
@@ -0,0 +1,60 @@
+class Stuff {
+  constructor(name, price, description, imageUrl, soldState, onlyAdult) {
+    this._name = name;
+    this._price = price;
+    this._description = description;
+    this._imageUrl = imageUrl;
+    this._soldState = soldState;
+    this._onlyAdult = onlyAdult;
+  }
+
+  getName() {
+    return this._name;
+  }
+
+  setName(newName) {
+    this._name = newName;
+  }
+
+  getPrice() {
+    return this._price;
+  }
+
+  setPrice(newPrice) {
+    this._price = newPrice;
+  }
+
+  getDescription() {
+    return this._Description;
+  }
+
+  setDescription(newDescription) {
+    this._description = newDescription;
+  }
+
+  getImageUrl() {
+    return this._imageUrl;
+  }
+
+  setImageUrl(newImageUrl) {
+    this._imageUrl = newImageUrl;
+  }
+
+  getSoldState() {
+    return this._soldState;
+  }
+
+  setSoldState(newSoldState) {
+    this._soldState = newSoldState;
+  }
+
+  getOnlyAdult() {
+    return this._onlyAdult;
+  }
+
+  setOnlyAdult(newOnlyAdult) {
+    this._onlyAdult = newOnlyAdult;
+  }
+}
+
+module.exports = Stuff;
